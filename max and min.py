9b61e5402edf345c6e05825fff16fa3f3e26be86
nethmi33 <hnakansha@students.nsbm.ac.lk>
2026-08-10 21:15:45 +0530

    Add files via upload

---
+++ max and min.py
@@ -0,0 +1,14 @@
+list=[45,12,78,23,67,89,34]
+max=list[0]
+min=list[0]
+for num in list:
+    if  num>max:
+        max=num
+    if num<min:
+        min=num
+
+print(f'max: {max} , min: {min}')
+
+
+                
+        
